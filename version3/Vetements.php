4c9516f816296a7902b2c9a730e270904ab8a615
XKoczan <koczanxavier@gmail.com>
2019-05-02 18:17:38 +0200

    Version3

---
+++ version3/Vetements.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'init.var.php';
+require 'header.php';
+    $req="SELECT * FROM cataloguemodele cm, catalogue c WHERE c.categorie LIKE 'vetement' AND c.idprod = cm.idprod ORDER BY cm.idprod ASC";
+    $qry = $db->prepare($req);
+    $qry->execute();
+    $items=$qry->fetchAll(PDO::FETCH_OBJ);
+$i=0;
+$produit=0;
+echo("<header><h1>Vetements</h1></header>");
+
+    foreach($items as $item)
+    {
+       if($item->stock!=0)
+        {
+           if($produit != $item->idprod && $i<1)
+           {
+              echo ("
+                <div class ='catalogue'>
+                    <div class='pic'><img src='$item->photos' style='height:100%; width:100%;'></div>
+                    <div class='nommodele'>$item->nom</div>
+                    <div class='prix'>$item->prix €</div>
+                    <div class='panierpic'>
+                        <form action='ajouterpanier.php' method ='post'>
+                        <input type='hidden' name='val' value='1'>
+                        <input type='hidden' name='nomfic' value='Vetements.php'>
+                        <button name='id' value ='$item->idprod' type='submit'><img src='panier.jpg' style='height:100%; width:100%;'>
+                        </button>
+                        </form>
+                    </div>
+                    <div class='desc'> Description du produit: $item->idprod</div>
+                </div>
+                ");  
+           }
+            
+        }
+        $produit=$item->idprod;
+    }
+require'footer.php';
+?>
---
+++ version3/panier.php
@@ -0,0 +1,58 @@
+<?php
+require 'init.var.php';
+require 'header.php';
+$prixtotal=0;
+echo("<header><h1>Votre Panier</h1></header>
+                <div class='panier'>
+                    <div class='tab'>
+                        <div class='tab1'>Image</div>
+                        <div class='tab1'>Nom</div>
+                        <div class='tab1'>Quantite</div>
+                        <div class='tab1'>Prix unit</div>
+                        <div class='tab1'>Prix lot</div>
+                    </div>
+                </div>");
+foreach($_SESSION as $elem)
+{
+    foreach($elem as $key=>$value)
+    {                      
+    $req="SELECT * FROM cataloguemodele cm, catalogue c WHERE c.idprod = cm.idprod AND idmodele =$key";
+    
+    $qry = $db->prepare($req);
+    $qry->execute();
+    $results=$qry->fetchAll(PDO::FETCH_OBJ); 
+    
+       foreach($results as $result)
+       {
+           $price=$result->prix*$value;
+           echo ("
+                
+                    <div class='elem'>
+                        <div class='elem1'>
+                            <td><img src='$result->photos' style='height:100%; width:100%;'></td><br>
+                        </div>
+                        <div class='elem1'>$result->nom</div>
+                        <div class='elem1'>$value</div>
+                        <div class='elem1'>$result->prix</div>
+                        <div class='elem1'>$price</div>
+                        
+                        <div class='supp'>
+                            <a href='supprimerpanier.php?id=$result->idmodele'><img src='del2.png' style='height:40%; width:20%;'></a><br>
+                        </div> 
+                        
+                    </div>
+
+                    
+               
+        
+                ");
+           $prixtotal+=$value*$result->prix;
+       }
+    }
+   
+    
+   echo "<h4>Prix total du panier :$prixtotal €" ;
+    echo"<h3><a href='Formulaire-panier.html'><input type='button' value='Acheter'></a></h3>";
+require'footer.php';
+}
+    
